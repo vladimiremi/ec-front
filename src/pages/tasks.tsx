52630ab484d2322b83747b4c88b96d7a1c3ff135
Cleyton-1995 <cleytoncosta.developer@gmail.com>
2024-05-25 11:21:53 -0300

    blue

--- src/pages/tasks.tsx
+++ src/pages/tasks.tsx
@@ -1,7 +1,7 @@
 export default function Tasks(){
     return(
         <div className="flex gap-2 justify-center">
-          <div className="bg-gray-400 border-b border-gray-700 p-3 flex justify-center w-full">
+          <div className="bg-blue-400 border-b border-gray-700 p-3 flex justify-center w-full">
             <div>27/05/2024</div>
             <div>Título</div>
           </div>
